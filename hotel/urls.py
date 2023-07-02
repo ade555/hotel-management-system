1346f765b961646c1c1d1ca0db5b44ffaf9e244f
superherojt <ademolathompson5@gmail.com>
2023-07-02 02:27:27 +0100

    Revert "users can no longer checkout at a back date"
    This reverts commit 49315151d9349e1a28378f9bdeb09d07e7edb020.

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,6 +3,6 @@ from .views import RoomListView, RoomDetailView, BookingListView
 
 urlpatterns = [
     path('rooms/', RoomListView.as_view(), name = 'RoomListView'),
-    path('rooms/<str:room_type>/', RoomDetailView.as_view(), name = 'RoomDetailView'),
+    path('rooms/<str:room_type>', RoomDetailView.as_view(), name = 'RoomDetailView'),
     path('booking_list/', BookingListView.as_view(), name='BookingListView'),
 ]

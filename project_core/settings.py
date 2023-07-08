8c963f5482d88b9eaafedd32c65b975145d47e84
superherojt <ademolathompson5@gmail.com>
2023-07-08 01:59:26 +0100

    fixing error 500 bug in production

--- project_core/settings.py
+++ project_core/settings.py
@@ -190,5 +190,6 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSOWORD")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
+CONTACT_EMAIL = os.environ.get("CONTACT_EMAIL")
 
 AUTH_USER_MODEL = 'users.User'

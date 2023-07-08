e4655139877e845ac00c4c692f4cc60b19c14bb3
superherojt <ademolathompson5@gmail.com>
2023-07-08 01:54:42 +0100

    fixing error 500 bug in production

--- project_core/settings.py
+++ project_core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'hotel-miramar-sg.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'hotel-miramar-sg.onrender.com', 'hotel-miramar-sg.designitafrica.org']
 
 # Application definition
 

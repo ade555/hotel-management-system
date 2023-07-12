37db00a2368ead9467e34ff1095489fa9c670499
superherojt <ademolathompson5@gmail.com>
2023-07-12 23:55:58 +0100

    modified build.sh file

--- build.sh
+++ build.sh
@@ -3,9 +3,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py makemigrations content_app
-python manage.py makemigrations hotel
-python manage.py makemigrations users
 python manage.py migrate
+python manage.py collectstatic --no-input

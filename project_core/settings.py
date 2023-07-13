65d3892b3529de265ec75a7615910c6c70f8dfe9
superherojt <ademolathompson5@gmail.com>
2023-07-13 12:43:19 +0100

    changed some styles and settings

--- project_core/settings.py
+++ project_core/settings.py
@@ -33,7 +33,7 @@ if(len(sys.argv)>= 2 and sys.argv[1]=='runserver'):
     SITE_ID = 3
 else:
     DEBUG = True
-    SITE_ID = 3
+    SITE_ID = 2
 
 ALLOWED_HOSTS = ['127.0.0.1', 'hotel-miramar-sg.onrender.com', 'hotel-miramar-sg.designitafrica.org']
 
@@ -109,23 +109,23 @@ WSGI_APPLICATION = 'project_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'PORT':5432,
-        'HOST':os.environ.get("DB_HOST"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("DB_NAME"),
+#         'USER': os.environ.get("DB_USER"),
+#         'PASSWORD': os.environ.get("DB_PASSWORD"),
+#         'PORT':5432,
+#         'HOST':os.environ.get("DB_HOST"),
+#     }
+# }
 
 
 # allauth configurations

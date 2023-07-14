b6875df75365323353e052a187f38563d1886a9e
superherojt <ademolathompson5@gmail.com>
2023-07-14 01:51:43 +0100

    configured default aws storage

--- project_core/settings.py
+++ project_core/settings.py
@@ -75,6 +75,9 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+
+    # storages
+    'storages',
 ]
 
 
@@ -193,8 +196,8 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -213,8 +216,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_SIGNATURE_NAME = os.environ.get('AWS_S3_SIGNATURE_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME ')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  os.environ.get('AWS_DEFAULT_ACL')
+AWS_DEFAULT_ACL =  os.environ.get('AWS_DEFAULT_ACL')
 AWS_S3_VERITY = os.environ.get('AWS_S3_VERITY')
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'project_core.storages.MediaStore'

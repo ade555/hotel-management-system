043ca039419bc3fc255c83bf457dfe6af056fa96
superherojt <ademolathompson5@gmail.com>
2023-07-16 01:38:38 +0100

    added site header and site title for the custom admin site

--- project_core/admin.py
+++ project_core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # create a new custom admin panel for the website admin
 class CustomAdmin(admin.AdminSite):
-    site_header = 'Hello'
+    site_header = 'Hotel Miramar SG Admin'
+    site_title = 'Hotel Miramar SG Admin Panel'
 
 custom_admin = CustomAdmin(name='custom-admin')
